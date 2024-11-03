3d42fc911ba8c47c4d6db9ab3640b8ac9c66fd09
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-11-02 23:33:08 -0300

    Creating order order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,8 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+
 import uploadConfig from './config/multer';
 
 const router = Router();
@@ -41,5 +43,8 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle);
 // -- ROTAS PRODUCT --
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
+
+// -- ROTAS ORDER -- 
+router.post('/order', isAuthenticated, new CreateOrderController().handle);
 
 export { router };
