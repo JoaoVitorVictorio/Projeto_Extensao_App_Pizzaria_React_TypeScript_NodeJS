784b7f7c78b096d4da5319fb214ab7f0011c6ef8
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-11-04 19:47:44 -0300

    Implementing the add items to order functionality

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -15,8 +15,10 @@ import { ListByCategoryController } from './controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
 
 import uploadConfig from './config/multer';
+import { AddItemService } from './services/order/AddItemService';
 
 const router = Router();
 
@@ -48,5 +50,6 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // -- ROTAS ORDER -- 
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+router.post('/order/add', isAuthenticated, new  AddItemController().handle);
 
 export { router };
