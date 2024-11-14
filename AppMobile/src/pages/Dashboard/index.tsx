1d5c1d632da0029316b2546277c961e6da4388e3
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-11-13 23:37:03 -0300

    Implemented the table opening functionality to generate a new order

--- AppMobile/src/pages/Dashboard/index.tsx
+++ AppMobile/src/pages/Dashboard/index.tsx
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
 
+import { useNavigation } from '@react-navigation/native'
+
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { StackPramsList } from '../../routes/app.routes'
+
 
 export default function Dashboard() {
+    const navigation = useNavigation<NativeStackNavigationProp<StackPramsList>>();
+
+    const [number, setNumber] = useState('');
+
+    async function openOrder() {
+        if (number === '') {
+            return;
+        }
+
+        navigation.navigate('Order', { number: number, order_id: '9557defc-bbd7-4af5-bc6c-ca18879ba328' })
+
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Novo pedido</Text>
@@ -12,9 +30,11 @@ export default function Dashboard() {
                 placeholderTextColor="#F0F0F0"
                 style={styles.input}
                 keyboardType="numeric"
+                value={number}
+                onChangeText={setNumber}
             />
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={openOrder}>
                 <Text style={styles.buttonText}>Abrir mesa</Text>
             </TouchableOpacity>
 
