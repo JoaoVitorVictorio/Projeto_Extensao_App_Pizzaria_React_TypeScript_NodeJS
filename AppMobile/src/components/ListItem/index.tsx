7528ccdc5e73747df7702ec4d60d14bd4ebb7a45
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-11-15 01:44:02 -0300

    Implemented the functionality to finalize the table order

--- AppMobile/src/components/ListItem/index.tsx
+++ AppMobile/src/components/ListItem/index.tsx
@@ -4,31 +4,31 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
 interface ItemProps {
-    data:{
-      id: string;
-      product_id: string;
-      name: string;
-      amount: string | number;
+    data: {
+        id: string;
+        product_id: string;
+        name: string;
+        amount: string | number;
     };
     deleteItem: (item_id: string) => void;
-  }
+}
 
-export function ListItem({ data, deleteItem }: ItemProps){
+export function ListItem({ data, deleteItem }: ItemProps) {
 
-    function handleDeleteItem(){
-      deleteItem(data.id)
+    function handleDeleteItem() {
+        deleteItem(data.id)
     }
-    
-    return(
-      <View style={styles.container}>
-        <Text style={styles.item}>{data.amount} - {data.name}</Text>
-        
-        <TouchableOpacity onPress={handleDeleteItem}>
-          <Feather name="trash-2" color="#FF3F4b" size={25} />
-        </TouchableOpacity>
-      </View>
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.item}>{data.amount} - {data.name}</Text>
+
+            <TouchableOpacity onPress={handleDeleteItem}>
+                <Feather name="trash-2" color="#FF3F4b" size={25} />
+            </TouchableOpacity>
+        </View>
     )
-  }
+}
 
 const styles = StyleSheet.create({
     container: {
