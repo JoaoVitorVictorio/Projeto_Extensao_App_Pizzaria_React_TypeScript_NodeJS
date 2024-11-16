b46030767f2127ee3fb1e5371488d923a1678446
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-11-16 19:44:53 -0300

    Logout functionality has been implemented on the dashboard screen, allowing the user to log in with another account.

--- AppMobile/src/pages/Dashboard/index.tsx
+++ AppMobile/src/pages/Dashboard/index.tsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
-
-import { useNavigation } from '@react-navigation/native'
-
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { StackPramsList } from '../../routes/app.routes'
-
+import React, { useState, useContext } from 'react';
+import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { StackPramsList } from '../../routes/app.routes';
 import { api } from '../../services/api';
-
+import { AuthContext } from '../../contexts/AuthContexts';
 
 export default function Dashboard() {
     const navigation = useNavigation<NativeStackNavigationProp<StackPramsList>>();
+    const { signOut } = useContext(AuthContext); // Importa o contexto de autenticação
 
     const [number, setNumber] = useState('');
 
@@ -21,9 +19,9 @@ export default function Dashboard() {
 
         const response = await api.post('/order', {
             table: Number(number)
-        })
+        });
 
-        navigation.navigate('Order', { number: number, order_id: response.data.id })
+        navigation.navigate('Order', { number: number, order_id: response.data.id });
 
         setNumber('');
     }
@@ -45,8 +43,12 @@ export default function Dashboard() {
                 <Text style={styles.buttonText}>Abrir mesa</Text>
             </TouchableOpacity>
 
+            {/* Botão para logout */}
+            <TouchableOpacity style={[styles.button, styles.logoutButton]} onPress={signOut}>
+                <Text style={styles.buttonText}>Sair</Text>
+            </TouchableOpacity>
         </SafeAreaView>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -86,5 +88,8 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: '#101026',
         fontWeight: 'bold'
-    }
-})
+    },
+    logoutButton: {
+        backgroundColor: '#FF6347', // Cor diferente para o botão de logout
+    },
+});
